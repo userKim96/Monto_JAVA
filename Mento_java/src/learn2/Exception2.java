4e2848dab63e319d52396d8c468f76bba47f719f
userKim96 <rlaclgnsska002@gmail.com>
2023-10-04 19:58:29 +0900

    예외처리-예외/예외처리 개념

---
+++ Mento_java/src/learn2/Exception2.java
@@ -0,0 +1,27 @@
+package learn2;
+
+public class Exception2 {
+	public static void main(String[] args) {
+		
+		try {
+			
+			int []a = {2,0};
+			int b = 4;
+			int c = b/a[2];
+			System.out.println(c);
+			
+		}
+		
+		catch(ArithmeticException e) {
+			System.out.println("XX산술오류XX");
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("XX산술오류XX");
+		}
+		System.out.println("==예외처리 테스트==");
+		
+		
+		
+	}
+
+}
---
+++ Mento_java/src/learn2/Finally.java
@@ -0,0 +1,20 @@
+package learn2;
+
+public class Finally {
+	public static void main(String[] args) {
+	
+		int a = 0;
+		int b = 2;
+		
+		try {
+			System.out.println("외부로 접속");
+			int c = b/a;
+		}
+		catch(ArithmeticException e) {
+			System.out.println("오류가 발생했습니다.");
+		}
+		finally {
+			System.out.println("연결해제");
+		}
+	}
+}
