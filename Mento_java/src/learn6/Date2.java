480a6bf926e853e634c6173a88138cc54b39c383
userKim96 <rlaclgnsska002@gmail.com>
2023-10-10 19:29:27 +0900

    java.util 패키지

---
+++ Mento_java/src/learn6/Date2.java
@@ -0,0 +1,14 @@
+package learn6;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Date2 {
+	public static void main(String[] args) {
+	Calendar a = Calendar.getInstance();
+	Calendar b = new GregorianCalendar();
+	System.out.println(a.toString());
+	System.out.println(b.toString());
+	
+	}
+}
---
+++ Mento_java/src/learn6/Date5.java
@@ -0,0 +1,18 @@
+package learn6;
+
+import java.util.Date;
+
+public class Date5 {
+
+	public static void main(String[] args) {
+
+		Date today = new Date();
+		System.out.println(today);
+		
+		long a = System.currentTimeMillis();
+		Date today2 = new Date(a);
+		System.out.println(today2);
+		
+	}
+
+}
