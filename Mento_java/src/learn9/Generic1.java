7db06c1eacc6833c89b2224d7da8de91687a990d
userKim96 <rlaclgnsska002@gmail.com>
2023-10-11 19:25:10 +0900

    컬렉션 프레임웍 1

---
+++ Mento_java/src/learn9/Generic1.java
@@ -0,0 +1,39 @@
+package learn9;
+
+public class Generic1 {
+
+	public static void main(String[] args) {
+		
+		Value v = new Value();
+		v.<Integer>typeSearch(3);
+		v.typeSearch(3.14);
+		v.typeSearch(3.14f);
+		v.typeSearch('a');
+		v.typeSearch("hello");
+		v.typeSearch(true);
+		
+	}
+
+}
+
+class Value {
+	public <T> void typeSearch(T x) {
+		if(x instanceof Integer) {
+			System.out.println(x + "는 정수입니다.");
+		}
+		else if(x instanceof Double || x instanceof Float) {
+			System.out.println(x + "는 실수입니다.");
+		}
+		else if(x instanceof Character) {
+			System.out.println(x + "는 문자형입니다.");
+		}
+		else if(x instanceof String) {
+			System.out.println(x + "는 문자열입니다.");
+		}
+		else if(x instanceof Boolean) {
+			System.out.println(x + "는 논리형입니다.");
+		}
+			
+		
+	}
+}
